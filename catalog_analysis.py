24f2ab1597b42df9254167938052b0c8e47b23d6
Nikolai Ulianov <kolyas18@gmail.com>
2026-09-17 20:57:36 +0300

    add iterators and generators

--- catalog_analysis.py
+++ catalog_analysis.py
@@ -180,3 +180,17 @@ def genres_only_in_one(movies_a, movies_b):
 
     return genres_a - genres_b
 
+def iter_high_rated(movies, min_rating=8.0):
+    for movie in movies:
+        if movie["rating"] >= min_rating:
+            yield movie
+
+
+for movie in iter_high_rated(movies):
+    print(format_report_line(movie))
+
+
+total_duration = sum(
+    movie["duration_min"] for movie in movies if movie["rating"] > 7
+)
+
